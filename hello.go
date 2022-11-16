1a212efb1f96f2cc48a19c4ec3ebef4a6297fc6a
N4choWasTaken <thierrygmuer@outlook.com>
2022-11-16 21:44:54 +0100

    Added learnings

--- hello.go
+++ hello.go
@@ -1,3 +1,4 @@
+// i followed this tutorial on github - https://github.com/quii/learn-go-with-tests/blob/main/hello-world.md
 package main
 
 import "fmt"
@@ -8,6 +9,9 @@ const englishHelloPrefix = "Hello "
 const spanishHelloPrefix = "Hola "
 const frenchHelloPrefix = "Bonjour "
 
+// functions that starts lowercase are private
+// functions that starts uppercase are public
+
 func Hello(name string, language string) string {
 
 	if name == "" {
@@ -17,7 +21,7 @@ func Hello(name string, language string) string {
 	return greetingPrefix(language) + name
 }
 
-func greetingPrefix(language string) (prefix string) {
+func greetingPrefix(language string) (prefix string) { // with (prefix string) we say that return should return prefix. So we don't need return prefix
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
