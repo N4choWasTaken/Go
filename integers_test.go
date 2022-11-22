36ebcf1e7ad1d0e1ca8b40ece9c4dbe40d54cc2c
N4choWasTaken <thierrygmuer@outlook.com>
2022-11-22 15:18:22 +0100

    Repeat has Benchmark

--- integers_test.go
+++ integers_test.go
@@ -12,3 +12,9 @@ func TestAdder(t *testing.T) {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
